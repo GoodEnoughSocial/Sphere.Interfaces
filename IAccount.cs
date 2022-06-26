0de5fe647de121817a52779d01ff767bdebd681b
Chad Jessup <chad.jessup@gmail.com>
2022-06-26 01:07:34 -0700

    basic userprofile and account grain interfaces

---
+++ IAccount.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using Orleans;
+
+namespace Sphere.Interfaces;
+
+public interface IAccount : IGrainWithStringKey
+{
+    public string Email { get; set; }
+    public CreatedWith CreatedWith { get; set; }
+    public string UserName { get; set; }
+    public long AccountId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string AccountDisplayName { get; set; }
+    public TimeZoneInfo TimeZoneInfo { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public IPAddress CreationIP { get; set; }
+    public string PhoneNumber { get; set; }
+}
+
+public enum CreatedWith
+{
+    Unknown = 0,
+    Web,
+    Mobile,
+}
---
+++ IUserProfile.cs
@@ -0,0 +1,13 @@
+﻿using Orleans;
+
+namespace Sphere.Interfaces;
+
+public interface IUserProfile : IGrainWithIntegerKey
+{
+    public Description Description { get; set; }
+    public string AvatarMediaUrl { get; set; }
+    public string HeaderMediaUrl { get; set; }
+}
+
+[Serializable]
+public record Description(string Bio, string Website, string Location);
